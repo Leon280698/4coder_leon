100f728c6998f98e9bc236ffa9abad7d35449299
Leon Buckel <leon@buckel-baehr.de>
2020-01-23 21:48:47 +0100

    Update for mac version

--- 4coder_leon.cpp
+++ 4coder_leon.cpp
@@ -5,8 +5,8 @@
 // TOP
 
 #include <cctype>
-#include "4coder_default_include.cpp"
-#include "4coder_default_map.cpp"
+#include <4coder_default_include.cpp>
+#include <generated/managed_id_metadata.cpp>
 
 #include "4coder_leon_whitespace.cpp"
 
@@ -33,7 +33,7 @@ CUSTOM_COMMAND_SIG(leon_write_text_and_auto_indent){
 	if(insert.str != 0 && insert.size > 0){
 		b32 do_auto_indent = false;
 
-		for (u64 i = 0; !do_auto_indent && i < insert.size; ++i){
+		for(u64 i = 0; !do_auto_indent && i < insert.size; ++i){
 			switch (insert.str[i]){
 				case ';': case ':':
 				case '{': case '}':
@@ -72,90 +72,6 @@ BUFFER_HOOK_SIG(leon_file_save){
 	return default_file_save(app, buffer_id);
 }
 
-function void
-leon_render_caller(Application_Links *app, Frame_Info frame_info, View_ID view_id){
-	ProfileScope(app, "leon render caller");
-	View_ID active_view = get_active_view(app, Access_Always);
-	b32 is_active_view = (active_view == view_id);
-
-	Rect_f32 region = draw_background_and_margin(app, view_id, is_active_view);
-	Rect_f32 prev_clip = draw_set_clip(app, region);
-
-	Buffer_ID buffer = view_get_buffer(app, view_id, Access_Always);
-	Face_ID face_id = get_face_id(app, buffer);
-	Face_Metrics face_metrics = get_face_metrics(app, face_id);
-	f32 line_height = face_metrics.line_height;
-	f32 digit_advance = face_metrics.decimal_digit_advance;
-
-	// NOTE(allen): file bar
-	b64 showing_file_bar = false;
-	if (view_get_setting(app, view_id, ViewSetting_ShowFileBar, &showing_file_bar) && showing_file_bar){
-		Rect_f32_Pair pair = layout_file_bar_on_top(region, line_height);
-		draw_file_bar(app, view_id, buffer, face_id, pair.min);
-		region = pair.max;
-	}
-
-	Buffer_Scroll scroll = view_get_buffer_scroll(app, view_id);
-
-	Buffer_Point_Delta_Result delta = delta_apply(app, view_id,
-												  frame_info.animation_dt, scroll);
-	if (!block_match_struct(&scroll.position, &delta.point)){
-		block_copy_struct(&scroll.position, &delta.point);
-		view_set_buffer_scroll(app, view_id, scroll, SetBufferScroll_NoCursorChange);
-	}
-	if (delta.still_animating){
-		animate_in_n_milliseconds(app, 0);
-	}
-
-	// NOTE(allen): query bars
-	{
-		Query_Bar *space[32];
-		Query_Bar_Ptr_Array query_bars = {};
-		query_bars.ptrs = space;
-		if (get_active_query_bars(app, view_id, ArrayCount(space), &query_bars)){
-			for (i32 i = 0; i < query_bars.count; i += 1){
-				Rect_f32_Pair pair = layout_query_bar_on_top(region, line_height, 1);
-				draw_query_bar(app, query_bars.ptrs[i], face_id, pair.min);
-				region = pair.max;
-			}
-		}
-	}
-
-	// NOTE(allen): FPS hud
-	if (show_fps_hud){
-		Rect_f32_Pair pair = layout_fps_hud_on_bottom(region, line_height);
-		draw_fps_hud(app, frame_info, face_id, pair.max);
-		region = pair.min;
-		animate_in_n_milliseconds(app, 1000);
-	}
-
-	// NOTE(allen): layout line numbers
-	Rect_f32 line_number_rect = {};
-	if (global_config.show_line_number_margins){
-		Rect_f32_Pair pair = layout_line_number_margin(app, buffer, region, digit_advance);
-		line_number_rect = pair.min;
-		region = pair.max;
-	}
-
-	// NOTE(allen): begin buffer render
-	Buffer_Point buffer_point = scroll.position;
-	Text_Layout_ID text_layout_id = text_layout_create(app, buffer, region, buffer_point);
-
-	// NOTE(allen): draw line numbers
-	if (global_config.show_line_number_margins){
-		draw_line_number_margin(app, view_id, buffer, face_id, text_layout_id, line_number_rect);
-	}
-
-	// NOTE(allen): draw the buffer
-	default_render_buffer(app, view_id, face_id, buffer, text_layout_id, region);
-
-	// NOTE(Leon): Begin custom rendering
-	// NOTE(Leon): End custom rendering
-
-	text_layout_free(app, text_layout_id);
-	draw_set_clip(app, prev_clip);
-}
-
 void
 custom_layer_init(Application_Links *app){
 	Thread_Context* tctx = get_thread_context(app);
@@ -172,10 +88,13 @@ custom_layer_init(Application_Links *app){
 	// NOTE(allen): default hooks and command maps
 	set_all_default_hooks(app);
 	set_custom_hook(app, HookID_SaveFile, leon_file_save);
-	set_custom_hook(app, HookID_RenderCaller, leon_render_caller);
 	mapping_init(tctx, &framework_mapping);
 
 	// Setting up default mapping
+
+#ifdef __APPLE__
+	#define KeyCode_Control KeyCode_Command
+#endif
 
 	MappingScope();
 	SelectMapping(&framework_mapping);
@@ -275,7 +194,9 @@ custom_layer_init(Application_Links *app){
 	Bind(duplicate_line,              KeyCode_L, KeyCode_Control);
 	Bind(cursor_mark_swap,            KeyCode_M, KeyCode_Control);
 	Bind(reopen,                      KeyCode_O, KeyCode_Control, KeyCode_Shift);
-	Bind(query_replace,               KeyCode_Q, KeyCode_Control);
+#ifdef __APPLE__
+	Bind(exit_4coder,                 KeyCode_Q, KeyCode_Control);
+#endif
 	Bind(query_replace_identifier,    KeyCode_Q, KeyCode_Control, KeyCode_Shift);
 	Bind(query_replace_selection,     KeyCode_Q, KeyCode_Alt);
 	Bind(reverse_search,              KeyCode_R, KeyCode_Control);
@@ -302,7 +223,6 @@ custom_layer_init(Application_Links *app){
 	Bind(move_left_alpha_numeric_or_camel_boundary,  KeyCode_Left, KeyCode_Alt);
 	Bind(move_right_alpha_numeric_or_camel_boundary, KeyCode_Right, KeyCode_Alt);
 	Bind(comment_line_toggle,        KeyCode_Semicolon, KeyCode_Control);
-	//Bind(word_complete,              KeyCode_Tab); // NOTE(Leon): Disable to be able to type tab characters
 	Bind(word_complete,  	        KeyCode_Tab, KeyCode_Control); // NOTE(Leon): Override
 	Bind(auto_indent_line_at_cursor, KeyCode_Tab, KeyCode_Shift);
 	Bind(word_complete_drop_down,    KeyCode_Tab, KeyCode_Shift, KeyCode_Control);
