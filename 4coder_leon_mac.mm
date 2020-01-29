d6c3e2cea39c6ab47f299e71c6f48177f8115212
Leon Buckel <leon@buckel-baehr.de>
2020-01-29 10:33:32 +0100

    Make sure 4coder window is active when launching

--- 4coder_leon_mac.mm
+++ 4coder_leon_mac.mm
@@ -1,10 +1,11 @@
 #include <Cocoa/Cocoa.h>
 
 void leon_mac_init(){
+	[NSApp activateIgnoringOtherApps:YES];
+
 	auto mainMenu = [NSMenu new];
 	auto appMenuItem = [NSMenuItem new];
 	auto appMenu = [NSMenu new];
-
 	auto quitMenuItem = [[NSMenuItem alloc] initWithTitle:@"Quit 4coder"
 											action:@selector(windowShouldClose:) // NOTE(Leon): Not calling terminate because there might be unsaved files which is handled by 4coder
 											keyEquivalent:@"q"];
