deb5a9cb5bebafa9d38727a80adf8a75227ef230
Leon Buckel <leon@buckel-baehr.de>
2020-03-11 19:11:14 +0100

    Fix build.bat for 4.1.4

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-call ..\bin\buildsuper_x64.bat 4coder_leon.cpp
+call ..\bin\buildsuper_x64-win.bat 4coder_leon.cpp
 copy custom_4coder.dll ..\..\custom_4coder.dll
 copy custom_4coder.pdb ..\..\custom_4coder.pdb
 copy vc140.pdb ..\..\vc140.pdb
